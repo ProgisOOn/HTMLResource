70273867cec808629c6eade79f8495faa06e24a7
Nikita <nburzhik@mail.ru>
2024-02-11 12:52:04 +0300

    Finished layout project

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -3,5 +3,15 @@ $(function(){
     arrows: false,
     fade: true,
     autoplay: 3000,
+    dots: true,
  });
+
+ $('.header-btn').on('click', function(){
+   $('.menu').addClass('active');
+ });
+
+ $('.close-btn').on('click', function(){
+   $('.menu').removeClass('active');
+ });
+
 });
